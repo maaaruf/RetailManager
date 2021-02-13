f6403df58f39935c1ad6b8d83ebbfb47794e496b
maaaruf <marufbdonline@gmail.com>
2021-02-13 17:59:19 +0600

    Added User

---
+++ RetailManager/RMData/dbo/Tables/User.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [AuthUserId] NCHAR(128) NOT NULL, 
+    [FirstName] NCHAR(50) NOT NULL, 
+    [LastName] NCHAR(50) NOT NULL, 
+    [EmailAddress] NCHAR(256) NOT NULL, 
+    [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+
+)
