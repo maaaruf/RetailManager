67a12a84d50cfe6887c11f8740bf832dcc693634
maaaruf <marufbdonline@gmail.com>
2021-02-14 10:15:30 +0600

    Remove AuthUserId

--- RetailManager/RMData/dbo/Tables/User.sql
+++ RetailManager/RMData/dbo/Tables/User.sql
@@ -1,7 +1,6 @@
 ﻿CREATE TABLE [dbo].[User]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [AuthUserId] NCHAR(128) NOT NULL, 
     [FirstName] NCHAR(50) NOT NULL, 
     [LastName] NCHAR(50) NOT NULL, 
     [EmailAddress] NCHAR(256) NOT NULL, 
